dbb908f8391094e7486a2e915d48e56959113253
thebungeechair <reagan.gropp@gmail.com>
2018-08-23 17:20:53 -0500

    ll

---
+++ name_major.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Reagan Gropp
+ * Date: 08/23/2018
+ *
+ * A name and major program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Reagan | Computer Science\n");
+
+  return 0;
+}
