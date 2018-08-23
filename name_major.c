46459eff6a4c8d57bc6e0034fd39791dfa9076fd
creid01 <42503765+creid01@users.noreply.github.com>
2018-08-23 17:59:04 -0500

    Fix the file

--- name_major.c
+++ name_major.c
@@ -1,5 +1,5 @@
 /**
- * Author: Christopher Reid
+ * Author: Christopher Reid and Reagan Gropp
  * Date: 08/23/2018
  *
  * A name and major program in C
@@ -11,6 +11,6 @@
 int main(int argc, char **argv) {
 
   printf("Christopher Reid | Computer Science\n");
-
+  printf("Reagan Gropp | Computer Science\n");
   return 0;
 }
